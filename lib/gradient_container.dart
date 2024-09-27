39cfd9e77f6bc11c28f14b86997a186b2847f2a3
Saransh <saransh@BMDVSARANSHSH-2.local>
2024-09-27 17:10:46 +0530

    clean up gradient container

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,13 +1,19 @@
 import 'package:dice/dice_roller.dart';
 import 'package:flutter/material.dart';
 
+const _startAlignment = Alignment.topLeft;
+const _endAlignment = Alignment.bottomRight;
 
-const startAlignment = Alignment.topLeft;
-const endAlignment = Alignment.bottomRight;
-
+/// A container with a gradient background.
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2, {super.key});
+  /// Creates a gradient container with custom colors.
+  const GradientContainer({
+    required this.color1,
+    required this.color2,
+    super.key,
+  });
 
+  /// Creates a gradient container with purple colors.
   const GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
@@ -15,19 +21,23 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
+  BoxDecoration _buildGradientDecoration() {
+    return BoxDecoration(
+      gradient: LinearGradient(
+        colors: [color1, color2],
+        begin: _startAlignment,
+        end: _endAlignment,
+      ),
+    );
+  }
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [color1, color2],
-            begin: startAlignment,
-            end: endAlignment,
-          ),
-        ),
-        child: const Center(
-          child: DiceRoller(),
-        ));
+      decoration: _buildGradientDecoration(),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
   }
 }
