5c0d47845760562de3866019136c682810e9ba7b
Saransh <saransh@BMDVSARANSHSH-2.local>
2024-09-27 17:06:14 +0530

    clean up dice roller

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -2,48 +2,46 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 class DiceRoller extends StatefulWidget {
-
   const DiceRoller({super.key});
 
   @override
-  State<DiceRoller> createState() {
-    
-    return _DiceRollerState();
-  }
+  State<DiceRoller> createState() => _DiceRollerState();
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  
-  var activeDiceImage = 'assets/images/dice-1.png';
-  final random = Random();
+  static const _basePath = 'assets/images/dice-';
+  var _activeDiceImage = '${_basePath}1.png';
+  final _randomGenerator = Random();
 
-  void rollDice() {
+  void _rollDice() {
+    final diceRoll = _randomGenerator.nextInt(6) + 1;
     setState(() {
-      var diceRoll = random.nextInt(6) + 1;
-      activeDiceImage = 'assets/images/dice-$diceRoll.png';
+      _activeDiceImage = '$_basePath$diceRoll.png';
     });
   }
 
+  ButtonStyle get _buttonStyle => TextButton.styleFrom(
+        padding: const EdgeInsets.only(top: 20),
+        foregroundColor: Colors.white,
+        textStyle: const TextStyle(fontSize: 32),
+      );
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                activeDiceImage,  // Use activeDiceImage here
-                width: 350,
-              ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: rollDice,
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.only(top: 20),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(fontSize: 32),
-                ),
-                child: const Text('Roll Dice'),
-              )
-            ],
-          );
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          _activeDiceImage,
+          width: 350,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: _rollDice,
+          style: _buttonStyle,
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
   }
 }
